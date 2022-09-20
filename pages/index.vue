5802b048556efc6bdd2e6147d5e2a8953d629047
BashirShalaik <bsalaik2010@gmail.com>
2022-09-20 21:23:24 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+<h1>
+	Home Page
+</h1>
+<h2>Reentaly viewd</h2>
+<h2>Work Spaces  </h2>
+<input type="text" v-model="nwsp" @keyup.enter="createworkspace">
+<button @click="createworkspace"> create workspace </button>
+<ul>
+	<li v-for="wsp in wops" :key="wsp.id">
+		{{wsp.id}} -- {{wsp.name}}
+	</li>
+</ul>
+</template>
+<script setup>
+import { ref } from 'vue'
+const nwsp = ref('')
+
+const createworkspace = () => {
+	wops.value.push({
+		id: Math.floor(Math.random() * 1000),
+		name: nwsp.value
+	})
+	nwsp.value=''
+}
+const wops = ref([
+	{ id: 120, name: "Test" },
+	{ id: 150, name: "New Work space" }
+	
+])
+</script>
+
+
